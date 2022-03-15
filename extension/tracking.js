74594dd27abca907cd2e726dbe98435bb1e453b4
jonnycross10 <crossjonny5@gmail.com>
2022-03-15 16:43:53 -0700

    got the js to log data

--- extension/tracking.js
+++ extension/tracking.js
@@ -1,4 +1,10 @@
 function alertFunction(){
-    var heading2 = document.getElementsByTagName('h2');
-    console.log(heading2[0]);
+    console.log('test1')
+    var heading1 = document.getElementsByTagName('h1');
+    heading1.innerHTML = 'js test';
+    console.log('test2')
 }
+
+var but = document.getElementById('button');
+but.addEventListener('click', alertFunction(), true);
+but.onclick = alertFunction()
