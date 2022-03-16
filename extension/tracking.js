9bf4084ed52b7a8e39c68c4762fa736799c6fbf4
jonnycross10 <crossjonny5@gmail.com>
2022-03-15 17:14:43 -0700

    added background script

--- extension/tracking.js
+++ extension/tracking.js
@@ -1,10 +1,3 @@
-function alertFunction(){
-    console.log('test1')
-    var heading1 = document.getElementsByTagName('h1');
-    heading1.innerHTML = 'js test';
-    console.log('test2')
-}
-
-var but = document.getElementById('button');
-but.addEventListener('click', alertFunction(), true);
-but.onclick = alertFunction()
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('hi');
+});
