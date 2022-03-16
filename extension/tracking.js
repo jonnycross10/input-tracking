acaa30e5accb29807cb868b316ff6f7104d17cf1
jonnycross10 <crossjonny5@gmail.com>
2022-03-15 18:47:04 -0700

    key events are now logged in background

--- extension/tracking.js
+++ extension/tracking.js
@@ -1,21 +1,35 @@
+//TODO initialize data structure to store keys
+
 chrome.runtime.onInstalled.addListener(() => {
+    //TODO set key storage var to empty
     console.log('hi');
     logKeys();
-    addRecvr();
+    keyRecvr();
 });
 
 function logKeys(){
     console.log("success");
 }
 
-function addRecvr() {
+// function addRecvr() {
+//     chrome.runtime.onMessage.addListener(
+//         function(request, sender, sendResponse) {
+//           console.log(sender.tab ?
+//                       "from a content script:" + sender.tab.url :
+//                       "from the extension");
+//           if (request.greeting === "hello")
+//             sendResponse({farewell: "goodbye"});
+//         }
+//       );
+// }
+
+function keyRecvr() {
     chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-          console.log(sender.tab ?
-                      "from a content script:" + sender.tab.url :
-                      "from the extension");
-          if (request.greeting === "hello")
-            sendResponse({farewell: "goodbye"});
+        function(request, sender,sendResponse){
+            //takes req, sender info, and sendResp function takes a message json
+            //TODO call a function and pass it key. The function should add it to the storage var
+            console.log(request.keyVal);
+            sendResponse({resp:"received key"})
         }
-      );
+    );
 }
